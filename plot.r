f7fc0e4536a5ab423bc72c5dad3e7a5d315a72c7
Natan7 <natan.ribeiro@ccc.ufcg.edu.br>
2018-12-18 16:09:38 -0300

    implementação funcionando com plot (estilo o LAB02)

---
+++ plot.r
@@ -0,0 +1,10 @@
+library("ggplot2")
+
+args = commandArgs(trailingOnly=TRUE)
+data = read.csv(args[1], header = TRUE)
+
+summary(data)
+
+p <- ggplot(data, aes(x=nForks, y=Tempo, color=Algoritmo))
+p <- p + geom_line() + coord_trans(y="log10")
+ggsave(p, file=paste(args[1], "png", sep="."))
--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 
-rm output/results.txt 2>/dev/null
+rm output/* 2>/dev/null
 mkdir output/ 2>/dev/null
 gcc main.c -o main 2>/dev/null
 echo "Running tests..."
-for sched in OTHER FIFO BATCH 
+for sched in OTHER BATCH FIFO
 do
-	./main $sched >> output/results.txt
+	./main $sched >> output/results.$sched.out
 done
 echo "Done!"
